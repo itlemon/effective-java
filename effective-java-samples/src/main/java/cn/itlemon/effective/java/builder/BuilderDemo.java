37b68eed8094600500d1bb08b99d266401d255bb
itlemon <lemon_jiang@aliyun.com>
2020-08-25 10:38:40 +0800

    【title】 Builder模式代码案例
    description: Builder模式代码案例

---
+++ effective-java-samples/src/main/java/cn/itlemon/effective/java/builder/BuilderDemo.java
@@ -0,0 +1,25 @@
+package cn.itlemon.effective.java.builder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cn.itlemon.effective.java.builder.NutritionFacts.Builder;
+
+/**
+ * @author jiangpingping
+ * Created on 2020-08-25
+ */
+public class BuilderDemo {
+
+    private static final Logger logger = LoggerFactory.getLogger(BuilderDemo.class);
+
+    public static void main(String[] args) {
+        NutritionFacts build = new Builder(10, 20)
+                .calories(10)
+                .fat(10)
+                .carbohydrate(20)
+                .sodium(5)
+                .build();
+        logger.info("result:{}", build);
+    }
+}
